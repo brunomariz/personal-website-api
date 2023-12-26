bc7599186d2615cd6d4bb3a478efe4a65f89d173
Bruno Mariz <bmarizot@gmail.com>
2023-12-25 22:55:34 -0300

    add reload to uvicorn

--- shell/build.sh
+++ shell/build.sh
@@ -4,4 +4,4 @@ docker rm -f pw-api
 docker rmi -f pw-api
 
 docker build -t pw-api .
-docker run -d --name pw-api -p ${1:-5000}:80 pw-api
+docker run -d --name pw-api -p ${1:-5000}:80 -v $PWD:/code pw-api
